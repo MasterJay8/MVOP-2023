32b16bd40ea3fd61dec02378d70a70592bf201bb
MasterJay8 <113784571+MasterJay8@users.noreply.github.com>
2023-03-06 14:20:34 +0000

    Design1

--- Klikacka/src/main.js
+++ Klikacka/src/main.js
@@ -1,5 +1,7 @@
 import { createApp } from 'vue'
-import './style.css'
+import './style/style.css'
+import './style/reset.css'
+//import './style.css'
 import App from './App.vue'
 
 createApp(App).mount('#app')
