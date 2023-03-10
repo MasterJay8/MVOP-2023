3ce3ccae3dd7b0a0ccd3e22aa9426e3d81e6ef17
MasterJay8 <jirka.kocour100@gmail.com>
2023-03-10 06:48:23 +0100

    Design1

--- Klikacka/src/App.vue
+++ Klikacka/src/App.vue
@@ -5,15 +5,54 @@
   <div class="all">
     <div class="leftWrap">
       <div class="coins">
-        120k
+        <div>120k</div>
+        <i class="fa-solid fa-coins"></i>
       </div>
-      <div class="shop">
-        shop
+
+      <div class="shopMenu">
+        <div class="shopNav">
+          <div class="shop">
+            <i class="fa-solid fa-bowl-rice"></i>
+          </div>
+
+          <div class="shop">
+             <img src="/src/img/chopsticks.png" alt=""/>
+          </div>
+
+          <div class="shop">
+            <img src="/src/img/noodle.png" alt=""/>
+          </div>
+        </div>
+
+        <div class="shopMain">
+          <div class="character">
+            <div class="img">
+              <img src="/src/img/Peaseant.jpg" alt=""/>
+            </div>
+            <div class="stats">lv500</div>
+            <div class="upgrade">500<i class="fa-solid fa-coins"></i></div>
+          </div>
+
+                    <div class="character">
+            <div class="img">
+              <img src="/src/img/asiaGang.png" alt=""/>
+            </div>
+            <div class="stats">lv500</div>
+            <div class="upgrade">500<i class="fa-solid fa-coins"></i></div>
+          </div>
+
+                    <div class="character">
+            <div class="img">
+              <img src="/src/img/asian3.png " alt=""/>
+            </div>
+            <div class="stats">lv500</div>
+            <div class="upgrade">500<i class="fa-solid fa-coins"></i></div>
+          </div>
+
+        </div>
       </div>
     </div>
-    <div class="rightWrap">
-      IMG
-    </div>
+    <div class="rightWrap">IMG</div>
   </div>
 </template>
 
